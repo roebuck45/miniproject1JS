a49d4e94f08ef9f99609ab2624e869c827858168
roebuck45 <j_seirer@mail.fhsu.edu>
2023-09-18 09:39:36 -0500

    cleaned up code and commets

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
-#(5/5 points) Initial comments with your name, class and project at the top of your .py file.
 # INF601 - Advanced Programming in Python
 # Joshua Seirer
 # Mini Project 1
 
-#(5/5 points) Proper import of packages used.
+# pip freeze > requirements.txt
+
 import yfinance as yf
 import numpy as np
 import matplotlib.pyplot as plt
 from pathlib import Path
 
-#(20/20 points) Using an API of your choice (yfinance works), collect the closing price of 5 of your favorite stock
-# tickers for the last 10 trading days.
 
+# tickers for the last 10 trading days.
 # Apple is is AAPL
 # Mircosoft is MSFT
 
@@ -25,7 +24,6 @@ def getClosing(ticker):
     for price in hist['Close']:
         closingList.append(round(price,2))
     return closingList
-
 def getStocks():
     stock = []
     print("Please pick 5 stocks")
@@ -41,8 +39,7 @@ def getStocks():
             except:
                 print("Not valdi")
 
-
-
+# Create our "Charts" directory
 try:
     Path("Charts").mkdir()
 except FileExistsError:
@@ -56,36 +53,24 @@ for stock in getStocks():
     # Plots graph
     plt.plot(days, mstfClosing)
 
-
+    # Get our min and max for Y
     prices = getClosing(stock)
     prices.sort()
     low_price = prices[0]
     high_price = prices[-1]
 
+    # Set out X axis min and max
     # form [xmin, xmax, ymin, ymax]
     plt.axis([1, 10, low_price-2, high_price+2])
 
-    # Sets x and y labels
+    # Set out labels for the graph
     plt.xlabel("Days")
     plt.ylabel("Closing Price")
     plt.title("Closing Price for " + stock)
 
+    # Saves plot
     saveFile = "Charts/" + stock + ".png"
     plt.savefig(saveFile)
 
+    # Show the the graph
     plt.show()
-
-
-
-
-#(10/10 points) Store this information in a list that you will convert to a ndarray in NumPy.
-#(10/10 points) Plot these 5 graphs. Feel free to add as much information to the graphs as you like exploring the
-# documentation for matplotlib. At minimum it just needs to show 10 data points.
-#(10/10 points) Save these graphs in a folder called charts as PNG files. Do not upload these to your project folder,
-# the project should save these when it executes. You may want to add this folder to your .gitignore file.
-#(10/10 points) There should be a minimum of 5 commits on your project, be sure to commit often!
-#(10/10 points) I will be checking out the master branch of your project. Please be sure to include a requirements.txt
-# file which contains all the packages that need installed. You can create this fille with the output of pip freeze at
-# the terminal prompt.
-#(20/20 points) There should be a README.md file in your project that explains what your project is, how to install
-# the pip requirements, and how to execute the program. Please use the GitHub flavor of Markdown.
